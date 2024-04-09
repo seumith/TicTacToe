d4bca586e67f04c251f7e08df6f32b62435121b2
seumith <thesumit91@gmail.com>
2024-04-09 15:39:31 +0530

    complete Bug fixes in withFriend

--- withFriend/withFriendScript.js
+++ withFriend/withFriendScript.js
@@ -8,118 +8,103 @@ let clearScore = document.querySelector(".clear")
 let gameWinner = document.querySelector('.game-winner')
 let gameWinnerContainer = document.querySelector('.show-winner')
 let userMoves = Array(9).fill(null)
-let chance = true
 let winner = ""
+let clicks = 0
 
-
-function clearTheBoard(){
-    userMoves = Array(9).fill(null)
-    // chance = !chance
-
-    if(chance){
-        turnViewer.innerHTML = "X"
-        turnViewer.style.backgroundColor = "rgb(187, 255, 191)"
-    }else{
-        turnViewer.innerHTML = "O"
-        turnViewer.style.backgroundColor = "rgb(255, 187, 187)"
-    }
-    cells.forEach((cell)=>{
-        cell.innerHTML = ""
-        cell.style.backgroundColor = "white"
-    })
-    gameWinnerContainer.style.visibility = "hidden"
-    
+function clearTheBoard() {
+  userMoves = Array(9).fill(null)
+  if (chance) {
+    turnViewer.innerHTML = "X"
+    turnViewer.style.backgroundColor = "rgb(187, 255, 191)"
+  } else {
+    turnViewer.innerHTML = "O"
+    turnViewer.style.backgroundColor = "rgb(255, 187, 187)"
+  }
+  cells.forEach((cell) => {
+    cell.innerHTML = ""
+    cell.style.backgroundColor = "white"
+  })
+  gameWinnerContainer.style.visibility = "hidden"
 }
 
-function getAndUpdateScores(){
-    xScores.innerHTML = `X : ${localStorage.getItem("xScore") || "0"}`
-    oScores.innerHTML = `O : ${localStorage.getItem("oScore") || "0"}`
-}
-function result(){
-    // let result = false
-    for(let i = 0;i < winningPattern.length;i++){
-        let [a,b,c] = winningPattern[i]
-            if(userMoves[a] && userMoves[a] == userMoves[b] && userMoves[b] === userMoves[c]){
-                winner = userMoves[a]
-                // result = true
-                setTimeout(()=>{clearTheBoard()},2000)
-                gameWinnerContainer.style.visibility = "visible"
-                return true
-            }
-        }
-        if(!userMoves.includes(null)){
-            gameWinnerContainer.style.visibility = "visible"
-            gameWinner.innerHTML = "Match Tied";
-            setTimeout(clearTheBoard,2000)
-            return false
-    }
-    
+function getAndUpdateScores() {
+  xScores.innerHTML = `X : ${localStorage.getItem("xScore") || "0"}`
+  oScores.innerHTML = `O : ${localStorage.getItem("oScore") || "0"}`
 }
 
-function setScore(){
-    if(winner == "X"){
-        localStorage.setItem("xScore",localStorage.getItem("xScore") ? +localStorage.getItem("xScore")+1 : 1)
-        gameWinner.innerHTML = "X is winner"
-        // chance = !chance
+function result() {
+  for (let i = 0; i < winningPattern.length; i++) {
+    let [a, b, c] = winningPattern[i]
+    if (userMoves[a] && userMoves[a] == userMoves[b] && userMoves[b] === userMoves[c]) {
+      winner = userMoves[a]
+      setTimeout(() => { clearTheBoard() }, 2000)
+      gameWinnerContainer.style.visibility = "visible"
+      return true
     }
-    if(winner == "O"){
-        localStorage.setItem("oScore",localStorage.getItem("oScore") ? +localStorage.getItem("oScore") +1 : 1)
-        gameWinner.innerHTML = "O is winner"
-        // chance = !chance
-    }
-    getAndUpdateScores()
+  }
+  if (!userMoves.includes(null)) {
+    gameWinnerContainer.style.visibility = "visible"
+    gameWinner.innerHTML = "Match Tied";
+    setTimeout(clearTheBoard, 2000)
+    return true
+  }
+  return false
 }
 
+getAndUpdateScores()
 
-clearScore.onclick = function(){
-    localStorage.clear()
-    getAndUpdateScores()
+function setScore() {
+  if (winner == "X") {
+    localStorage.setItem("xScore", localStorage.getItem("xScore") ? +localStorage.getItem("xScore") + 1 : 1)
+    gameWinner.innerHTML = "X is winner"
+  }
+  if (winner == "O") {
+    localStorage.setItem("oScore", localStorage.getItem("oScore") ? +localStorage.getItem("oScore") + 1 : 1)
+    gameWinner.innerHTML = "O is winner"
+  }
+  getAndUpdateScores()
+}
+
+clearScore.onclick = function () {
+  localStorage.clear()
+  getAndUpdateScores()
 };
-
 resetBoard.onclick = clearTheBoard;
 
 let winningPattern = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,4,8],
-    [2,4,6],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8]
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8]
 ]
 
-
-// getAndUpdateScores();
+let chance = true
 let results = false
+
 cells.forEach((cell) => {
-    let clicks = 2
-    cell.addEventListener("click",(e)=>{
-        const currentCellIndex = cells.indexOf(cell)
-        clicks -= 1
-        if(chance){
-            userMoves[currentCellIndex] == null && (e.target.innerHTML = "X") && (userMoves[currentCellIndex] = "X") && (turnViewer.innerHTML = "O") && (cell.style.backgroundColor = "rgb(187, 255, 191)") && (turnViewer.style.backgroundColor = "rgb(255, 187, 187)");
-            if(clicks == 1){
-                chance = !chance
-            };
-        }else{
-            userMoves[currentCellIndex] == null && (e.target.innerHTML = "O") && (userMoves[currentCellIndex] = "O") && (turnViewer.innerHTML = "X") && (cell.style.backgroundColor = "rgb(255, 187, 187)") && (turnViewer.style.backgroundColor = "rgb(187, 255, 191)");
-
-            if(clicks == 1){
-                chance = !chance
-            };
-        }
-        
-        if(clicks==1){
-            if(result()){
-                setScore()
-            }
-        }
-        getAndUpdateScores();
-    })
+  cell.addEventListener("click", (e) => {
+    const currentCellIndex = cells.indexOf(cell)
+    if (userMoves[currentCellIndex] === null) {
+      if (chance) {
+        e.target.innerHTML = "X"
+        userMoves[currentCellIndex] = "X"
+        cell.style.backgroundColor = "rgb(187, 255, 191)"
+      } else {
+        e.target.innerHTML = "O"
+        userMoves[currentCellIndex] = "O"
+        cell.style.backgroundColor = "rgb(255, 187, 187)"
+      }
+      chance = !chance
+      turnViewer.innerHTML = chance ? "X" : "O"
+      turnViewer.style.backgroundColor = chance ? "rgb(187, 255, 191)" : "rgb(255, 187, 187)";
+      if (result()) {
+        setScore()
+      }
+    }
+    getAndUpdateScores();
+  })
 });
-
-
-
-
-// export {clearTheBoard,setScore,}
